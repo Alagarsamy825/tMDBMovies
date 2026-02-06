4a5817ce49350d4a04d84cbda38b168136b8ebac
Alagarsamy <alagarsamy825@gmail.com>
2026-02-07 04:02:39 +0530

    Initial Commit

---
+++ tMDBMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tMDBMovies
+//
+//  Created by Alagarsamy on 07/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
